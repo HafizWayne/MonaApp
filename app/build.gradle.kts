edfd09abf3a9621cdd8e9f5189abf304384a087c
HafizWayne <mhafidzdaffa42@gmail.com>
2024-05-31 10:43:56 +0700

    nav bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation("com.etebarian:meow-bottom-navigation:1.2.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
